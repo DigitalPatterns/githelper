4624a841d4e1b435c17b19e6bc7ab06091352583
Mark Olliver <mark@digitalpatterns.io>
2018-07-02 12:37:20 +0100

    update back

--- scripts/backupMongo.sh
+++ scripts/backupMongo.sh
@@ -6,20 +6,20 @@ export FORMIO_DEPLOYMENT_NAME="${FORMIO_DEPLOYMENT_NAME:-formio}"
 export MONGO_CONTAINER_NAME="${MONGO_CONTAINER_NAME:-mongo}"
 export REPO_URL="${REPO_URL}"
 export KUBECTL_SERVER="${KUBECTL_SERVER}"
-alias k="kubectl --server ${KUBECTL_SERVER} --token ${KUBECTL_TOKEN} --insecure-skip-tls-verify=true"
-export FORMIO_POD=$(k --namespace ${KUBECTL_NAMESPACE} get pods | grep ${FORMIO_DEPLOYMENT_NAME} | cut -f 1 -d " ")
+export k="kubectl --server ${KUBECTL_SERVER} --token ${KUBECTL_TOKEN} --insecure-skip-tls-verify=true"
+export FORMIO_POD=$($k --namespace ${KUBECTL_NAMESPACE} get pods | grep ${FORMIO_DEPLOYMENT_NAME} | cut -f 1 -d " ")
 
 env
 
 clone.sh
 
-k cp /bin/exportMongo.sh ${KUBECTL_NAMESPACE}/${FORMIO_POD}:/tmp/exportMongo.sh --container ${MONGO_CONTAINER_NAME}
-k --namespace ${KUBECTL_NAMESPACE} exec -it ${FORMIO_POD} --container ${MONGO_CONTAINER_NAME} '/bin/bash -c "chmod +x /tmp/exportMongo.sh; /tmp/exportMongo.sh"'
+$k cp /bin/exportMongo.sh ${KUBECTL_NAMESPACE}/${FORMIO_POD}:/tmp/exportMongo.sh --container ${MONGO_CONTAINER_NAME}
+$k --namespace ${KUBECTL_NAMESPACE} exec -it ${FORMIO_POD} --container ${MONGO_CONTAINER_NAME} '/bin/bash -c "chmod +x /tmp/exportMongo.sh; /tmp/exportMongo.sh"'
 
 cd /repo
-k cp ${KUBECTL_NAMESPACE}/${FORMIO_POD}:/tmp/forms.tar.gz forms.tar.gz --container ${MONGO_CONTAINER_NAME}
+$k cp ${KUBECTL_NAMESPACE}/${FORMIO_POD}:/tmp/forms.tar.gz forms.tar.gz --container ${MONGO_CONTAINER_NAME}
 tar zxvf forms.tar.gz
 git commit -a forms/* -c "Auto Mongo FormIO Backup"
 git push origin backup
 
-k --namespace ${KUBECTL_NAMESPACE} exec -it ${FORMIO_POD} --container ${MONGO_CONTAINER_NAME} "rm /tmp/forms.tar.gz"
+$k --namespace ${KUBECTL_NAMESPACE} exec -it ${FORMIO_POD} --container ${MONGO_CONTAINER_NAME} "rm /tmp/forms.tar.gz"
